3c55dd230e5a5dc6dee7f3bf88bf76487b5e15ac
milesmoralespro <145787286+milesmoralespro@users.noreply.github.com>
2025-10-31 17:51:36 +0530

    Add files via upload

---
+++ interfaces/PassengerCarrier.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import exceptions.OverloadException;
+import exceptions.InvalidOperationException;
+
+public interface PassengerCarrier {
+    void boardPassengers(int count) throws OverloadException;
+    void disembarkPassengers(int count) throws InvalidOperationException;
+    int getPassengerCapacity();
+    int getCurrentPassengers();
+}
