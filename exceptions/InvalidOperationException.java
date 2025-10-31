e95fe531dca59003bf93ddc2be8001bfab1a663e
milesmoralespro <145787286+milesmoralespro@users.noreply.github.com>
2025-10-31 17:50:35 +0530

    Add files via upload

---
+++ exceptions/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class InvalidOperationException extends Exception {
+    public InvalidOperationException(String msg) {
+        super(msg);
+    }
+}
---
+++ exceptions/OverloadException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class OverloadException extends Exception {
+    public OverloadException(String msg) {
+        super(msg);
+    }
+}
